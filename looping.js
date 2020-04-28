3535b22561c2fb36c6734e88f47e6238e3335494
JCBoyd <jcboyd@pm.me>
2020-04-28 08:52:03 -0600

    Initial commit, save to repository

---
+++ looping.js
@@ -0,0 +1,15 @@
+let number = 10;
+console.log("FOR loop");
+for (let index = 1; index <= 5; index ++) {
+    number += index;
+    console.log(number);
+}
+
+number = 10;
+let index = 1;
+console.log("WHILE loop");
+while (index <= 5) {
+    number += index;
+    index ++;
+    console.log(number);
+}
